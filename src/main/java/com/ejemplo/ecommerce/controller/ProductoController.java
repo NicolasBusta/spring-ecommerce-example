2f533a5a2f57c7fa384cc2ea0d1fa046dcd5da7c
NicolasBusta <nicolasbustamanteb1@gmail.com>
2024-10-22 13:26:35 -0300

    funcionalidad para editar productos

--- src/main/java/com/ejemplo/ecommerce/controller/ProductoController.java
+++ src/main/java/com/ejemplo/ecommerce/controller/ProductoController.java
@@ -1,10 +1,13 @@
 package com.ejemplo.ecommerce.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -39,6 +42,23 @@ public class ProductoController {
 		Usuario u = new Usuario(1,"","","","","","","");
 		producto.setUsuario(u);
 		productoService.save(producto);
+		return "redirect:/productos";
+	}
+	
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto=new Producto();
+		Optional<Producto> optionalProducto= productoService.get(id); //ESTO ES LO QUE NOS DEVUELVE AL BUSCAR UN OBJETO
+		producto= optionalProducto.get();
+		
+		LOGGER.info("PRODUCTO BUSCADO: {}", producto);
+		model.addAttribute("producto", producto);
+		return "productos/edit";
+	}
+	
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoService.update(producto);
 		return "redirect:/productos";
 	}
 	
--- src/main/java/com/ejemplo/ecommerce/service/ProductoService.java
+++ src/main/java/com/ejemplo/ecommerce/service/ProductoService.java
@@ -8,7 +8,7 @@ import com.ejemplo.ecommerce.model.Producto;
 public interface ProductoService {
 
 	public Producto save(Producto producto); 
-	public Optional<Producto> get(Integer id); //opcion d evalidar si el objeto existe o no en la DB.
+	public Optional<Producto> get(Integer id); //opcion de validar si el objeto existe o no en la DB.
 	public void update(Producto producto);
 	public void delete(Integer id);
 	public List<Producto> findAll();
